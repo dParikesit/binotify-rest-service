7dd9044ff5cf7fbe594556c1d5f109ef4ac667a4
dParikesit <dsparikesit@gmail.com>
2022-11-30 14:04:36 +0700

    feat: soap getPending, accept reject sub

--- src/controllers/soap.js
+++ src/controllers/soap.js
@@ -28,4 +28,54 @@ const reqSubscribe = async(req, res) => {
     }
 };
 
-module.exports = { reqSubscribe };
+const getPending = async (req, res) => {
+    try {
+        let client = await soap.createClientAsync(url);
+        await client.addSoapHeader(token);
+        const describe = await client.describe();
+        // console.log(describe['SubServiceService']['SubServicePort']);
+        let result = await client.getPendingAsync({});
+        let item = JSON.parse(result[0]['return']);
+        console.log(item)
+
+        await res.status(200).json(item);
+    } catch (error) {
+        console.log(error)
+        const err = parser.parse(error['body']);
+        console.log(err['S:Envelope']['S:Body']['S:Fault']['faultstring']);
+
+        await res.status(500).json('Get failed');
+    }
+};
+
+const acceptSubscribe = async (req, res) => {
+    const content = req.body;
+    try {
+        let client = await soap.createClientAsync(url);
+        await client.addSoapHeader(token);
+        let result = await client.acceptAsync(content);
+        await res.status(200).json(result[0]['return']);
+    } catch (error) {
+        const err = parser.parse(error['body']);
+        console.log(err['S:Envelope']['S:Body']['S:Fault']['faultstring']);
+
+        await res.status(500).json('Accept failed');
+    }
+};
+
+const rejectSubscribe = async (req, res) => {
+    const content = req.body;
+    try {
+        let client = await soap.createClientAsync(url);
+        await client.addSoapHeader(token);
+        let result = await client.rejectAsync(content);
+        await res.status(200).json(result[0]['return']);
+    } catch (error) {
+        const err = parser.parse(error['body']);
+        console.log(err['S:Envelope']['S:Body']['S:Fault']['faultstring']);
+
+        await res.status(500).json('Reject failed');
+    }
+}
+
+module.exports = { reqSubscribe, getPending, acceptSubscribe, rejectSubscribe };
--- src/routes/subscription.js
+++ src/routes/subscription.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { reqSubscribe } = require('../controllers/soap');
+const { reqSubscribe, getPending, acceptSubscribe, rejectSubscribe } = require('../controllers/soap');
 
 router.post('/new', reqSubscribe);
+router.get('/pending', getPending);
+router.put('/accept', acceptSubscribe);
+router.put('/reject', rejectSubscribe);
 
 module.exports = router;
