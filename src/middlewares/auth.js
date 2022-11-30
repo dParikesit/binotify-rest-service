c83c45d7bc1bc3b4962b5bfe55d28c6a6a71c97e
dParikesit <dsparikesit@gmail.com>
2022-11-30 21:54:20 +0700

    fix: subs endpoint enforce admin

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -1,5 +1,5 @@
 function authenticateAdmin(req, res, next) {
-    if (req.user.isAdmin) {
+    if (req.cookies.isAdmin) {
         next();
     } else {
         res.status(403).send({message: "You are not authorized to perform this action"});
